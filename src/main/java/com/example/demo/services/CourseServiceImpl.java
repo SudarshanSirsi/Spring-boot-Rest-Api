07b4074c8f1c694cc567fa07043b739f3afbbefc
SudarshanSirsi <sudarshanhegde247@gmail.com>
2023-06-18 19:43:56 +0530

    Achieve Database connection

--- src/main/java/com/example/demo/services/CourseServiceImpl.java
+++ src/main/java/com/example/demo/services/CourseServiceImpl.java
@@ -1,66 +1,47 @@
 package com.example.demo.services;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dao.CourseDao;
 import com.example.demo.entities.Course;
 
 @Service
 public class CourseServiceImpl implements CourseService {
 
-	List<Course> list;
-
-	public CourseServiceImpl() {
-		list = new ArrayList();
-		list.add(new Course(123, "Core java",
-				"All core java concepts including object oriented programming concepts are there"));
-		list.add(new Course(326, "Spring framework", "Spring IOC, Spring MVC and Spring Boot are there"));
-	}
+	@Autowired
+	private CourseDao courseDao;
 
 	@Override
 	public List<Course> getCourses() {
 
-		return list;
+		return courseDao.findAll();
 	}
 
 	@Override
 	public Course getCourseById(long courseID) {
-		Course c = null;
-		for (Course course : list) {
-			if (course.getId() == courseID) {
-				c = course;
-				break;
-			}
-		}
-		return c;
+		return courseDao.findById(courseID).get();
 	}
 
 	public Course addCourse(Course course) {
-		list.add(course);
+		courseDao.save(course);
 		return course;
 	}
 
 	@Override
 	public Course updateCourseById(Course course, long parseLong) {
-		for (Course course2 : list) {
-			if (course2.getId() == course.getId()) {
-				int i = list.indexOf(course2);
-				list.set(i, course);
-				break;
-			}
-		}
+
+		courseDao.save(course);
 		return course;
 	}
 
 	public Course deleteCourseById(long id) {
-		for (Course course : list) {
-			if(course.getId()==id) {
-				return list.remove(list.indexOf(course));
-			}
-		}
-		return null;
+		Course entity = courseDao.getReferenceById(id);
+		courseDao.delete(entity);
+		return entity;
 	}
 
 }
